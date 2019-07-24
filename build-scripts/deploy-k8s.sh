f40f519b9bdc48059f329306872195de7e74dd38
tpettit <38570808+tpettit@users.noreply.github.com>
2019-07-23 22:41:39 -0400

    Update deploy-k8s.sh

--- build-scripts/deploy-k8s.sh
+++ build-scripts/deploy-k8s.sh
@@ -3,16 +3,16 @@ GREEN='\033[0;32m'
 NC='\033[0;0m'
 export PATH=$PATH:$(pwd)
 echo -e "${GREEN}==== Deploying RBAC role ====${NC}"
-cd ../rbac/
+cd rbac/
 for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f --validate=false; done
 echo -e "${GREEN}==== Done deploying RBAC role ====${NC}"
 echo -e "${GREEN}==== Deploying iam role ====${NC}"
-cd ../kube2iam/
+cd kube2iam/
 for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f --validate=false; done
 echo -e "${GREEN}==== Done deploying iam role ====${NC}"
 echo ''
 echo -e "${GREEN}==== Deploying external dns ====${NC}"
-cd ../external_dns/
+cd external_dns/
 for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f --validate=false; done
 echo -e "${GREEN}==== Done deploying external dns ====${NC}"
 echo ''
