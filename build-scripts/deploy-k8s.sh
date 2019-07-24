9bfb6d5bf4d54597d731221de4efc1be489304be
Avi <avinash.setty@gmail.com>
2019-07-23 21:22:20 -0400

    Updating Travis config to deploy k8s config

--- build-scripts/deploy-k8s.sh
+++ build-scripts/deploy-k8s.sh
@@ -6,3 +6,13 @@ echo -e "${GREEN}==== Deploying RBAC role ====${NC}"
 cd ../rbac/
 for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f --validate=false; done
 echo -e "${GREEN}==== Done deploying RBAC role ====${NC}"
+echo -e "${GREEN}==== Deploying iam role ====${NC}"
+cd ../kube2iam/
+for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f --validate=false; done
+echo -e "${GREEN}==== Done deploying iam role ====${NC}"
+echo ''
+echo -e "${GREEN}==== Deploying external dns ====${NC}"
+cd ../external_dns/
+for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f --validate=false; done
+echo -e "${GREEN}==== Done deploying external dns ====${NC}"
+echo ''
